416bacf5ffc4feb4fea3142e9f3f7b1427174ced
Luis fernando chambi orosco <lchambio@unsa.edu.pe>
2022-07-14 18:53:26 -0500

    update

--- src/main/java/com/example/project/Exercise2.java
+++ src/main/java/com/example/project/Exercise2.java
@@ -22,6 +22,30 @@ public class Exercise2 {
 
     public <T extends Comparable<T>> boolean bstIguales(BST<T> a1, BST<T> a2){
 
-        return false;
+    
+    	if (a1.isEmpty() && a2.isEmpty()) { 
+    		return true; //si ambos están vacios retoan true
+    	}
+    	else //usaremos un metodo recursivo para los nodos 
+    		return nodeIguales(a1.root, a2.root) && nodeIguales(a1.root, a2.root);  
+    	    //el  subarboles izquierdo y derecho son iguales seran bst iguales
     }
-}
+    private <T extends Comparable<T>> boolean nodeIguales(Node<T> a1, Node<T> a2){ //Metodo recursivo 
+    	if(a1 == null && a2 == null) { //Si los nodos son iguales a null
+                return nodeIguales(a1.left, a2.left) && nodeIguales(a1.right, a2.right); 
+    		}
+    		else { 
+    			if(a1.data != a2.data) { //si los nodos son diferentes retirnara false
+    			return false; 
+    		}
+    		
+    	}
+    	if((a1 != null && a2 == null) || (a1 == null && a2 != null)) { //Si un nodo es nulo y el otro tiene valor los bst no son iguales
+    		return false;
+    	}
+    	else 
+    		return true;
+    }
+
+    }
+
